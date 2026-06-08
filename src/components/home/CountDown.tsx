042cb5854c1304c216d21700943be3cc88e1f29e
sahilyadav-15 <s6906433@gmail.com>
2026-06-08 20:37:44 +0530

    c

--- src/components/home/CountDown.tsx
+++ src/components/home/CountDown.tsx
@@ -1,16 +1,24 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 "use client";
 
+import { useEffect, useState } from "react";
 import Countdown from "react-countdown";
 
 const endingDate = new Date("2026-06-15");
 
 export default function CountDown() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
-    <div className="">
-      <Countdown
-        className="font-bold text-5xl text-yellow-300"
-        date={endingDate}
-      />
-    </div>
+    <Countdown
+      className="font-bold text-5xl text-yellow-300"
+      date={endingDate}
+    />
   );
 }
